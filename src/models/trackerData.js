c81e9fdb3c37f7083342d9ac769e2972d62c774a
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2023-05-12 19:32:20 -0400

    chore: apply prettier to models

--- src/models/trackerData.js
+++ src/models/trackerData.js
@@ -1,53 +1,53 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const trackerDataSchema = new mongoose.Schema({
-    latitude: {
-        type: Number
-    },
-    longitude: {
-        type: Number
-    },
-    location: {
-        type: JSON
-    },
-    altitude: {
-        type: Number
-    },
-    epe: {
-        type: Number
-    },
-    timeToFix: {
-        type: Number
-    },
-    isFreshGPS: {
-        type: Boolean
-    },
-    gpsTime: {
-        type: Number
-    },
-    vbat: {
-        type: Number
-    },
-    temp: {
-        type: Number
-    },
-    resetCounter: {
-        type: Number
-    },
-    deviceName: {
-        type: String
-    },
-    packageTime: {
-        type: Date
-    },
-    deviceEUI: {
-        type: String
-    },
-    rssi: {
-        type: Number
-    }
-})
+  latitude: {
+    type: Number,
+  },
+  longitude: {
+    type: Number,
+  },
+  location: {
+    type: JSON,
+  },
+  altitude: {
+    type: Number,
+  },
+  epe: {
+    type: Number,
+  },
+  timeToFix: {
+    type: Number,
+  },
+  isFreshGPS: {
+    type: Boolean,
+  },
+  gpsTime: {
+    type: Number,
+  },
+  vbat: {
+    type: Number,
+  },
+  temp: {
+    type: Number,
+  },
+  resetCounter: {
+    type: Number,
+  },
+  deviceName: {
+    type: String,
+  },
+  packageTime: {
+    type: Date,
+  },
+  deviceEUI: {
+    type: String,
+  },
+  rssi: {
+    type: Number,
+  },
+});
 
-const TrackerData = mongoose.model('squirrelData', trackerDataSchema)  // String in quotes is collection name in mongoDB
+const TrackerData = mongoose.model("squirrelData", trackerDataSchema); // String in quotes is collection name in mongoDB
 
-module.exports = TrackerData
+module.exports = TrackerData;
